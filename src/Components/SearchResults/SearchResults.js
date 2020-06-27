b0225dd394edd077637b3b48dbd4dd79fcc91926
majklfly <majklfly@gmail.com>
2020-06-27 21:51:29 +0100

    Track list render error

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './SearchResults.css';
-import TrackList from '../TrackList/TrackList';
+import React from "react";
+import "./SearchResults.css";
+import TrackList from "../TrackList/TrackList";
 
-class SearchResults extends React.Component {
-    render() {
-        return(
-            <div className="SearchResults">
-                <h2>Results</h2>
-                <TrackList  tracks={this.props.searchResults}
-                            onAdd={this.props.onAdd}/>
-            </div>
-        );
-    }
-}
+const SearchResults = props => {
+  console.log("results", props.searchResults);
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+    </div>
+  );
+};
 
 export default SearchResults;
