388a45703fd164d30cb0749398bacd12ea6effc8
majklfly <majklfly@gmail.com>
2020-07-03 22:17:19 +0100

    frontpage updated

--- src/Screens/HomeScreen/index.js
+++ src/Screens/HomeScreen/index.js
@@ -30,77 +30,82 @@ const HomeScreen = () => {
     state.token && getCurrentPlayback(state.token);
   };
 
-  return (
-    <div>
-      {state.token && (
-        <>
-          <div className="TopContainer">
-            <h1>
-              Ja<span className="highlight">mm</span>ing
-            </h1>
-            {state.userdata && (
-              <div className="userBoardInfo">
-                <div onClick={() => setShowUserPlaylist(!showUserPlaylist)}>
-                  {showUserPlaylist ? (
-                    <FontAwesomeIcon
-                      icon={faHome}
-                      className="ShowPlayerListIcon"
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faCompactDisc}
-                      className="ShowPlayerListIcon"
-                    />
-                  )}
-                </div>
-                <h3>{state.userdata.display_name}</h3>
-                <h5>
-                  {state.userdata.product} |{" "}
-                  {state.userdata.followers.total !== 1
-                    ? state.userdata.followers.total + " followers"
-                    : state.userdata.followers.total + " follower"}
-                </h5>
-                <a href={state.userdata.external_urls.spotify}>
-                  <img
-                    className="userBoardIMG"
-                    src={state.userdata.images[0].url}
-                    alt="profile_img"
+  if (state.token) {
+    return (
+      <>
+        <div className="TopContainer">
+          <h1>
+            Ja<span className="highlight">mm</span>ing
+          </h1>
+          {state.userdata && (
+            <div className="userBoardInfo">
+              <div onClick={() => setShowUserPlaylist(!showUserPlaylist)}>
+                {showUserPlaylist ? (
+                  <FontAwesomeIcon
+                    icon={faHome}
+                    className="ShowPlayerListIcon"
                   />
-                </a>
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faCompactDisc}
+                    className="ShowPlayerListIcon"
+                  />
+                )}
               </div>
-            )}
-          </div>
+              <h3>{state.userdata.display_name}</h3>
+              <h5>
+                {state.userdata.product} |{" "}
+                {state.userdata.followers.total !== 1
+                  ? state.userdata.followers.total + " followers"
+                  : state.userdata.followers.total + " follower"}
+              </h5>
+              <a href={state.userdata.external_urls.spotify}>
+                <img
+                  className="userBoardIMG"
+                  src={state.userdata.images[0].url}
+                  alt="profile_img"
+                />
+              </a>
+            </div>
+          )}
+        </div>
 
-          <div className="App">
-            <div className="Header">
-              <SearchBar />
-              <PlayerDataContainer />
-            </div>
-            <AnimatePresence>
-              {state.error && (
-                <motion.div
-                  className="errorMessage"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 0.5 }}
-                  transition={{ duration: 1 }}
-                  exit={{ opacity: 0 }}
-                >
-                  <p style={{ opacity: 0 }}>
-                    {setTimeout(cleanErrorMessage, 3000)}
-                  </p>
-                  <p>{state.error}</p>
-                </motion.div>
-              )}
-            </AnimatePresence>
-            <div className="App-playlist">
-              <SearchResults />
-              {showUserPlaylist ? <UserPlayLists /> : <Playlist />}
-            </div>
+        <div className="App">
+          <div className="Header">
+            <SearchBar />
+            <PlayerDataContainer />
           </div>
-        </>
-      )}
-    </div>
-  );
+          <AnimatePresence>
+            {state.error && (
+              <motion.div
+                className="errorMessage"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.5 }}
+                transition={{ duration: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <p style={{ opacity: 0 }}>
+                  {setTimeout(cleanErrorMessage, 3000)}
+                </p>
+                <p>{state.error}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div className="App-playlist">
+            <SearchResults />
+            {showUserPlaylist ? <UserPlayLists /> : <Playlist />}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Missing Token</h1>
+        {console.log(state)}
+      </div>
+    );
+  }
 };
 
 export default HomeScreen;
