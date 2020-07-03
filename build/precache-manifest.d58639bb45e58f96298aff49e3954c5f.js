c389a48c27c1c23c2f7d422b88db6b9c09f8320c
majklfly <majklfly@gmail.com>
2020-07-03 21:42:43 +0100

    build added to git

---
+++ build/precache-manifest.d58639bb45e58f96298aff49e3954c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cb66728e6ea07a91398f2c0f7a68da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2972a9666754a8c0807",
+    "url": "/static/css/main.b2823332.chunk.css"
+  },
+  {
+    "revision": "0479bd2a629ff4ebbfcb",
+    "url": "/static/js/2.b097affd.chunk.js"
+  },
+  {
+    "revision": "ddeb2b8065ed6bac550225a3fc05933b",
+    "url": "/static/js/2.b097affd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2972a9666754a8c0807",
+    "url": "/static/js/main.c031f3e7.chunk.js"
+  },
+  {
+    "revision": "120373d10e26e9062395",
+    "url": "/static/js/runtime-main.5f82ff70.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
